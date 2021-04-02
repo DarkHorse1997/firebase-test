9a4bc67ad80ea5f6737ff0e5d3ad78fe8e464197
darkhorse1997 <tanmoydas1997@gmail.com>
2021-04-03 01:21:43 +0530

    first and last commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAwe2v9rrhep_u_1i5FO5JRCqAcmtVDch8",
+  authDomain: "casual-carpool.firebaseapp.com",
+  projectId: "casual-carpool",
+  storageBucket: "casual-carpool.appspot.com",
+  messagingSenderId: "1001492116824",
+  appId: "1:1001492116824:web:930a20ccab916bce85a12d",
+};
+
+export default firebaseConfig;
